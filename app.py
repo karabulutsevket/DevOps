878eab2d7c8a88ba16b0ab3a4cea8c5bb0904efb
karabulutsevket <153216934+karabulutsevket@users.noreply.github.com>
2024-02-01 02:59:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Merhaba ben Sevket Karabulut')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:black'>Merhaba ben Sevket Karabulut! Bu bir Flask web app.</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
